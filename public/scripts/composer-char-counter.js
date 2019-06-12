d063e3a236b424014b6f04279c1635292e0b0110
Nikita Jiandani <niki.jiandani@gmail.com>
2019-06-12 14:40:51 -0400

    Create jquery to target & update counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  //targets the textarea in the new-tweet class on input and updates the counter to reflect the number of characters remaining. If the number of characters entered is greater than 140, it toggles the red class on the counter.
+  $(".new-tweet textarea").on("input", function() {
+    const counter = $(this).parent().children(".counter");
+    const textareaLength = $(this).val().length;
+    counter.text(140 - textareaLength);
+    if(textareaLength > 140){
+      counter.toggleClass("red", true);
+    } else {
+      counter.toggleClass("red", false);
+    }
+  })
+});
