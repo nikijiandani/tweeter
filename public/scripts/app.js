439b6e881af22f6fa281ca1fdc98713371e113df
Nikita Jiandani <niki.jiandani@gmail.com>
2019-06-13 11:43:42 -0400

    get rid of fakedata + Add tweet rendering w/sort

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,54 +5,6 @@
  */
 
 $(document).ready(function() {
-  // Test / driver code (temporary). Eventually will get this from the server.
-  // Fake data taken from tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-        "content": {
-          "text": "Je pense , donc je suis"
-        },
-        "created_at": 1461113959088
-      },
-      {
-        "user": {
-          "name": "Johann von Goethe",
-          "avatars": {
-            "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-            "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-            "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-          },
-          "handle": "@johann49"
-        },
-        "content": {
-          "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-        },
-        "created_at": 1461113796368
-      }
-    ];
 
     //gets the time since the tweet was created taking a UNIX timestamp from the tweet as input
     function convertTimestampIntoDays (date) {
@@ -93,13 +45,36 @@ $(document).ready(function() {
       `)
     }
     
-    var $tweets = renderTweets(data);
-    // Test / driver code (temporary)
-    console.log($tweets); // to see what it looks like
-    
     function renderTweets (tweetArr) {
       tweetArr.forEach(element => {
         $('#tweet-container').append(createTweetElement(element));
       });
     }
+
+    //Ajax request
+    $(function() {
+      var form = $('#form');
+      form.on('submit', function () {
+        event.preventDefault();
+        console.log('Form submitted, performing ajax call...');
+        console.log($(this).serialize());
+        $.ajax('/tweets/', { method: 'POST', data: $(this).serialize() })
+        .then((newTweet) => {
+          console.log('Success: ', newTweet);
+          $('#tweet-container').prepend(createTweetElement(newTweet));
+        });
+      });
+    });
+
+    $.ajax({
+      type: 'GET',
+      url: '/tweets/',
+      dataType: 'json',
+      success: function(res){
+        //if request made successfully
+        console.log(res);
+        console.log("This is the AJAX GET request!!!");
+        $('#tweet-container').prepend(renderTweets(res));
+      }
+    })
   });
