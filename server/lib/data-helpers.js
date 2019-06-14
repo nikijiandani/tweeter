bbaedb75bafa7008e8204e7ec25248744d4c6cf5
Nikita Jiandani <niki.jiandani@gmail.com>
2019-06-14 17:06:15 -0400

    Add data persistence to tweeter using mongodb

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -6,21 +6,20 @@ const simulateDelay = require("./util/simulate-delay");
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
-
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      db.collection("tweets").insertOne(newTweet, callback(null, true));
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if(err) {
+          return callback(err);
+        }
         const sortNewestFirst = (a, b) => b.created_at - a.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+        callback(null, tweets.sort(sortNewestFirst));
+      })
     }
 
   };
